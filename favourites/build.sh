73fa0cfe46db136bfb3b7ff72b0e9edc986241da
alifeee <alifeee.web@outlook.com>
2025-01-01 20:25:16 +0100

    modify workflows

---
+++ favourites/build.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+# generate HTML from markdown,
+#  then replace all lines between START_CONTENT and END_CONTENT with this HTML
+#  in the file INDEX_FILE
+# requires python markdown extension
+#  pip3 install --user markdown
+#  example:
+#    python3 -m markdown -x def_list images.md
+# usage:
+#  ./build.sh content.md
+
+set -e
+
+date >> /dev/stderr
+
+# check markdown is installed
+python3 -c "import markdown" && echo "python markdown is instaled!" > /dev/stderr || (echo "markdown not installed, run: pip3 install --user markdown" && exit 1)
+
+echo "found" $(find . -name "*.md" | wc -l) "files" >> /dev/stderr
+
+LISTFILE="/tmp/list2i8yt7gw.html"
+contents="<ul>"
+echo "" > $LISTFILE
+for file in *.md; do
+  # notify script runner
+  echo "  transforming ${file}" >> /dev/stderr
+
+  # get name without file extensions
+  sectionname=$(basename "${file%.*}")
+
+  # add to contents
+  contents="${contents}<li><a href=\"#${sectionname}\">${sectionname}</a></li>"
+
+  # create section
+  echo "<section>" >> $LISTFILE
+  echo '<h2 id="'"${sectionname}"'">'"${sectionname}"'</h2>' >> $LISTFILE
+  python3 -m markdown -x def_list "${file}" >> $LISTFILE
+  echo "</section>" >> $LISTFILE
+  echo "<hr />" >> $LISTFILE
+done
+
+contents="${contents}</ul><hr />"
+
+INDEX_FILE="index.html"
+TEMP_FILE="/tmp/index827fhu.html"
+START_CONTENT="<!-- !!start content!! -->"
+END_CONTENT="<!-- !!end content!! -->"
+
+original_html=$(cat $INDEX_FILE)
+
+html=$(cat $LISTFILE)
+
+# get line numbers of START_CONTENT and END_CONTENT
+start=$(echo "${original_html}" | grep -n "${START_CONTENT}" | cut -d : -f1)
+end=$(echo "${original_html}" | grep -n "${END_CONTENT}" | cut -d : -f1)
+
+if [ -z $start ] || [ -z $end ]; then
+  echo "could not find start/end content tags" >> /dev/stderr
+  echo "start: <$start>, end: <$end>" >> /dev/stderr
+  exit 1
+fi
+
+rm -f $TEMP_FILE
+echo "${original_html}" | awk 'NR <= '"${start}"'' >> $TEMP_FILE
+echo "${contents}" >> $TEMP_FILE
+echo "${html}" >> $TEMP_FILE
+echo "${original_html}" | awk 'NR >= '"${end}"'' >> $TEMP_FILE
+
+cat $TEMP_FILE > $INDEX_FILE
+rm -f $TEMP_FILE
+
+echo "done! saved to ${INDEX_FILE} 🚀" >> /dev/stderr
---
+++ favourites/images.md
@@ -0,0 +1,2 @@
+![drawing of several birds and a banner that says 'fuck their borders'](./images/borders.png)
+: I just think it's neat. The source is <https://wiki.xxiivv.com/media/refs/borders.png>
